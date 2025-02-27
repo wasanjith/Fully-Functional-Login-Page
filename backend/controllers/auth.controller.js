93b1a3736597f9c742e260a552a2472b658d9b0c
wasanjith <wasakodi79@gmail.com>
2025-02-27 20:44:04 +0530

    Signup controller created successfully.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,46 @@
+import { User } from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+
 export const signup = async (req, res) => {
-    res.send("Signup route");
-}
+    const { name, email, password } = req.body;
+    try {
+        if (!email || !password || !name) {
+            return res.status(400).json({ success: false, message: "All fields are required" });
+        }
+
+        const userAlreadyExists = await User.findOne({ email });
+        if (userAlreadyExists) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 12);
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+        });
+
+        await user.save();
+
+        generateTokenAndSetCookie(res, user._id);
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user: {
+                ...user.toObject(),
+                password: undefined,
+            },
+        });
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message || "An error occurred during signup" });
+    }
+};
 
 export const login = async (req, res) => {
     res.send("Login route");
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => { 
     res.send('Server is ready pakooo');
 });
