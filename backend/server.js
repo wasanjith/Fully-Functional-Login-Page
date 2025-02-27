47ac996d33d9f400b74bc3e87117ddbcb929007c
wasanjith <wasakodi79@gmail.com>
2025-02-27 10:52:51 +0530

    Create user model

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => { 
     res.send('Server is ready pakooo');
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
     connectDB();
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on ${PORT}`);
 });
